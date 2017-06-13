3980935549a08dca8d13da359ee68010ee42716e
marion <marionfontaine904@gmail.com>
2017-06-13 11:40:19 +0200

    Just the camera prevue on a SurfaceView with the VideoEncoder

--- StreamingAPI/app/src/main/java/com/example/marion/streamingapi/MainActivity.java
+++ StreamingAPI/app/src/main/java/com/example/marion/streamingapi/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.marion.streamingapi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private String TAG = "MainActivity";
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start =(Button) findViewById(R.id.buttonStart);
+        start.setOnClickListener(this);
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v==start){
+            startActivity(new Intent(MainActivity.this,StreamingActivity.class));
+        }
+    }
+
+    public void share(){
+
+    }
+
+
+
+
+
 }
---
+++ StreamingAPI/app/src/main/java/com/example/marion/streamingapi/VideoEncoderCore.java
@@ -0,0 +1,157 @@
+package com.example.marion.streamingapi;
+
+/**
+ * Created by marion on 13/06/17.
+ */
+
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
+import android.util.Log;
+import android.view.Surface;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ * This class wraps up the core components used for surface-input video encoding.
+ * <p>
+ * Once created, frames are fed to the input surface.  Remember to provide the presentation
+ * time stamp, and always call drainEncoder() before swapBuffers() to ensure that the
+ * producer side doesn't get backed up.
+ * <p>
+ * This class is not thread-safe, with one exception: it is valid to use the input surface
+ * on one thread, and drain the output on a different thread.
+ */
+public class VideoEncoderCore {
+    private static final String TAG = "VideoEncoderCore";
+    private static final boolean VERBOSE = true;
+
+    // TODO: these ought to be configurable as well
+    private static final String MIME_TYPE = "video/avc";    // H.264 Advanced Video Coding
+    private static final int FRAME_RATE = 30;               // 30fps
+    private static final int IFRAME_INTERVAL = 2;           // 2 seconds between I-frames
+
+    private Surface mInputSurface;
+    private MediaMuxer mMuxer;
+    private MediaCodec mEncoder;
+    private MediaCodec.BufferInfo mBufferInfo;
+    private int mTrackIndex;
+    private boolean mMuxerStarted, rtmpMuxerStarted;
+
+
+    /**
+     * Configures encoder and muxer state, and prepares the input Surface.
+     */
+    public VideoEncoderCore(int width, int height, int bitRate, String urlRtmp)
+            throws IOException {
+        mBufferInfo = new MediaCodec.BufferInfo();
+
+        MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, width, height);
+
+        // Set some properties.  Failing to specify some of these can cause the MediaCodec
+        // configure() call to throw an unhelpful exception.
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT,
+                MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        format.setInteger(MediaFormat.KEY_BIT_RATE, bitRate);
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);
+        if (VERBOSE) Log.d(TAG, "format: " + format);
+
+        // Create a MediaCodec encoder, and configure it with our format.  Get a Surface
+        // we can use for input and wrap it with a class that handles the EGL work.
+        mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
+        mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mInputSurface = mEncoder.createInputSurface();
+        mEncoder.start();
+
+        // Create a MediaMuxer.  We can't add the video track and start() the muxer here,
+        // because our MediaFormat doesn't have the Magic Goodies.  These can only be
+        // obtained from the encoder after it has started processing data.
+        //
+        // We're not actually interested in multiplexing audio.  We just want to convert
+        // the raw H.264 elementary stream we get from MediaCodec into a .mp4 file.
+
+        mMuxer = new MediaMuxer(urlRtmp, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+
+
+        mTrackIndex = -1;
+        mMuxerStarted = false;
+    }
+
+    /**
+     * Returns the encoder's input surface.
+     */
+    public Surface getInputSurface() {
+        return mInputSurface;
+    }
+
+    /**
+     * Releases encoder resources.
+     */
+    public void release() {
+        if (VERBOSE) Log.d(TAG, "releasing encoder objects");
+        if (mEncoder != null) {
+            mEncoder.stop();
+            mEncoder.release();
+            mEncoder = null;
+        }
+
+        if (mMuxerStarted) {
+            // TODO: stop() throws an exception if you haven't fed it any data.  Keep track
+            //       of frames submitted, and don't call stop() if we haven't written anything.
+            mMuxer.stop();
+            mMuxer.release();
+            mMuxer = null;
+        }
+    }
+
+    /**
+     * Extracts all pending data from the encoder and forwards it to the muxer.
+     * <p>
+     * If endOfStream is not set, this returns when there is no more data to drain.  If it
+     * is set, we send EOS to the encoder, and then iterate until we see EOS on the output.
+     * Calling this with endOfStream set should be done once, right before stopping the muxer.
+     * <p>
+     * We're just using the muxer to get a .mp4 file (instead of a raw H.264 stream).  We're
+     * not recording audio.
+     */
+    public void drainEncoder(boolean endOfStream) {
+        final int TIMEOUT_USEC = 10000;
+        ByteBuffer[] encoderOutputBuffers = mEncoder.getOutputBuffers();
+        while (true) {
+            int encoderStatus = mEncoder.dequeueOutputBuffer(mBufferInfo, TIMEOUT_USEC);
+            if (encoderStatus == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
+                // not expected for an encoder
+                encoderOutputBuffers = mEncoder.getOutputBuffers();
+                break;
+            } else {
+                if (mBufferInfo.size != 0) {
+                    final boolean isHeader = (mBufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0;
+
+                    final boolean isKeyframe = (mBufferInfo.flags & MediaCodec.BUFFER_FLAG_KEY_FRAME) != 0 && !isHeader;
+
+                    final long timestamp = mBufferInfo.presentationTimeUs;
+
+                    final ByteBuffer encodedData = encoderOutputBuffers[encoderStatus];
+                    // adjust the ByteBuffer values to match BufferInfo (not needed?)
+                    encodedData.position(mBufferInfo.offset);
+                    encodedData.limit(mBufferInfo.offset + mBufferInfo.size);
+
+                    // Extract video data
+                    final byte[] b = new byte[mBufferInfo.size];
+                    encodedData.get(b);
+                }
+
+                mEncoder.releaseOutputBuffer(encoderStatus, false);
+
+                if ((mBufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+                    break;      // out of while
+                }
+            }
+        }
+
+
+    }
+}
